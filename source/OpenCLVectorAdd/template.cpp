b620b6a437af37ffc21a79e0468932fdf1ee502d
Gupta <rahulgupta@csus.edu>
2025-05-08 15:25:31 -0700

    Add host code

--- source/OpenCLVectorAdd/template.cpp
+++ source/OpenCLVectorAdd/template.cpp
@@ -9,6 +9,7 @@
 //								"next_line_of_code  \n"
 //								...
 //								"last_line_of_code  \n";
+const char* kernalSource;
 
 int main(int argc, char *argv[]) {
   wbArg_t args;
@@ -51,32 +52,55 @@ int main(int argc, char *argv[]) {
   wbLog(TRACE, "The input size is ", inputLengthBytes, " bytes");
 
   //@@ Initialize the workgroup dimensions
+  cl_int clerr = CL_SUCCESS;
+  cl_platform_id cpPlatform;    // OpenCL platform
+  cl_device_id device_id;       // device ID
 
   //@@ Bind to platform
+  clerr = clGetPlatformIDs(1, &cpPlatform, NULL);
 
   //@@ Get ID for the device
+  clerr = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
 
   //@@ Create a context
+  cl_context clctx = clCreateContext(0, 1, &device_id, NULL, NULL, &clerr);
 
   //@@ Create a command queue
+  cl_command_queue clcmdq = clCreateCommandQueue(clctx, device_id, 0, &clerr);
 
   //@@ Create the compute program from the source buffer
+  cl_program clpgm;
+  clpgm = clCreateProgramWithSource(clctx, 1, &kernalSource, NULL, &clerr);
 
   //@@ Build the program executable
+  clerr = clBuildProgram(clpgm, 0, NULL, NULL, NULL, NULL);
 
   //@@ Create the compute kernel in the program we wish to run
+  cl_kernel clkern = clCreateKernel(clpgm, "kernelSource", &clerr);
 
   //@@ Create the input and output arrays in device memory for our calculation
-
   //@@ Write our data set into the input array in device memory
+  deviceInput1 = clCreateBuffer(clctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputLengthBytes, hostInput1, NULL);
+  deviceInput2 = clCreateBuffer(clctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputLengthBytes, hostInput2, NULL);
+  deviceOutput = clCreateBuffer(clctx, CL_MEM_WRITE_ONLY, inputLengthBytes, NULL, NULL);
 
   //@@ Set the arguments to our compute kernel
+  clerr = clSetKernelArg(clkern, 0, sizeof(cl_mem), (void *)&deviceInput1);
+  clerr = clSetKernelArg(clkern, 1, sizeof(cl_mem), (void*)&deviceInput2);
+  clerr = clSetKernelArg(clkern, 2, sizeof(cl_mem), (void*)&deviceOutput);
+  clerr = clSetKernelArg(clkern, 3, sizeof(int), &inputLength);
 
   //@@ Execute the kernel over the entire range of the data set
+  cl_event event = NULL;
+  const size_t Gsz = ((inputLength-1) / 256 + 1) * 256;
+  const size_t Bsz = 256;
+  clerr = clEnqueueNDRangeKernel(clcmdq, clkern, 1, NULL, &Gsz, &Bsz, 0, NULL, &event);
 
   //@@ Wait for the command queue to get serviced before reading back results
+  clerr = clWaitForEvents(1, &event);
 
   //@@ Read the results from the device
+  clEnqueueReadBuffer(clcmdq, deviceOutput, CL_TRUE, 0, inputLengthBytes, hostOutput, 0, NULL, NULL);
 
   wbSolution(args, hostOutput, inputLength);
 
